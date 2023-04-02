e4081acbb098c6fef5f4fa651f726d36212f4085
Anubhav Sharma <annu992002@gmail.com>
2023-04-02 18:09:56 +0530

    api

--- src/Components/formpage/inner-component/form.jsx
+++ src/Components/formpage/inner-component/form.jsx
@@ -1,5 +1,6 @@
 import { Button } from '@mui/material';
 import React, { useState } from 'react';
+import { SUBMITFORM } from '../../../utils/services';
 import Popup from './popup';
 
 function MyForm() {
@@ -13,18 +14,39 @@ function MyForm() {
     const [birthMonth, setBirthMonth] = useState('');
     const [birthYear, setBirthYear] = useState('');
     const [researchPaper, setResearchPaper] = useState(null);
+    const [filename, setfilename] = useState("")
+    const [loading, setloading] = useState(false)
 
-    const handleSubmit = (event) => {
+    const form = new FormData();
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        console.log(researchPaper);
+        setloading(true)
+        form.append("firstname", firstName)
+        form.append("lastname", lastName)
+        form.append("email", email)
+        form.append("password", password)
+        form.append("mobileNumber", mobileNumber)
+        form.append("dob", birthDay + birthMonth + birthYear)
+        form.append("myfile", researchPaper)
+        console.log("hello");
+        await SUBMITFORM(form).then((res) => {
+            handleOpen()
+            setloading(false)
+        }).catch((err) => {
+            console.log(err);
+            setloading(false)
+        })
+
         // handle form submission
     };
     const handleButtonClick = () => {
         document.getElementById('fileInput').click();
-        
+
     };
 
     const handleFileChange = (event) => {
+        setfilename(event.target.files[0].name);
         setResearchPaper(event.target.files[0]);
     };
     const [open, setOpen] = React.useState(false);
@@ -32,110 +54,125 @@ function MyForm() {
     const handleClose = () => setOpen(false);
     return (
         <>
-        <form onSubmit={handleSubmit} className="form-container">
-            <div>
-                <label>Full Name:</label>
+            <form className="form-container">
                 <div>
-                    <input
-                        type="text"
-                        value={firstName}
-                        onChange={(event) => setFirstName(event.target.value)}
-                        placeholder="First Name"
-                    />
-                    <input
-                        type="text"
-                        value={lastName}
-                        onChange={(event) => setLastName(event.target.value)}
-                        placeholder="Last Name"
-                    />
+                    <label>Full Name:</label>
+                    <div>
+                        <input
+                            type="text"
+                            value={firstName}
+                            onChange={(event) => setFirstName(event.target.value)}
+                            placeholder="First Name"
+                        />
+                        <input
+                            type="text"
+                            value={lastName}
+                            onChange={(event) => setLastName(event.target.value)}
+                            placeholder="Last Name"
+                        />
+                    </div>
                 </div>
-            </div>
-            <br />
-            <div>
-                <label>Email Address:</label>
-                <input
-                    type="email"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                />
-            </div>
-            <br />
-            <div className='password'>
-                <div >
-                    <label>Password:</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(event) => setPassword(event.target.value)}
-                    />
-                </div>
+                <br />
                 <div>
-                    <label>Repeat Password:</label>
+                    <label>Email Address:</label>
                     <input
-                        type="password"
-                        value={repeatPassword}
-                        onChange={(event) => setRepeatPassword(event.target.value)}
+                        placeholder='Email Address'
+                        type="email"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
                     />
                 </div>
-            </div>
-            <br />
-            <div>
-                <label>Mobile Number:</label>
-                <input
-                    type="tel"
-                    value={mobileNumber}
-                    onChange={(event) => setMobileNumber(event.target.value)}
-                />
-            </div>
-            <br />
-            <div>
-                <label>Date of Birth:</label>
-                <div>
-                    <input
-                        type="text"
-                        value={birthDay}
-                        onChange={(event) => setBirthDay(event.target.value)}
-                        placeholder="DD"
-                        maxLength="2"
-                    />
-                    <input
-                        type="text"
-                        value={birthMonth}
-                        onChange={(event) => setBirthMonth(event.target.value)}
-                        placeholder="MM"
-                        maxLength="2"
-                    />
-                    <input
-                        type="text"
-                        value={birthYear}
-                        onChange={(event) => setBirthYear(event.target.value)}
-                        placeholder="YYYY"
-                        maxLength="4"
-                    />
-                </div>
-            </div>
-            <br />
-            <div>
-                <label>Research Paper (.pdf)*:</label>
-                <input
-                    type="file"
-                    id="fileInput"
-                    onChange={handleFileChange}
-                    accept=".pdf"
-                    style={{ display: 'none' }}
-                />
-                <Button className="nav-button" onClick={handleButtonClick} >Upload File</Button>
+                <br />
+                <div className='password'>
+                    <div >
+                        <label>Password:</label>
+                        <input
+                            placeholder='Password'
+                            type="password"
+                            value={password}
+                            onChange={(event) => setPassword(event.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label>Repeat Password:</label>
+                        <input
+                            placeholder='Repeat Password'
 
-            </div>
-            <br />
-            <span>Note: Kindly fill the details carefully as it will be printed on the award trophy, certificates, etc.</span>
-            <br />
-            <div>
-                <Button className="nav-button" id="submit-button" onClick={handleOpen}>Submit Application</Button>
-            </div>
-        </form>
+                            type="password"
+                            value={repeatPassword}
+                            onChange={(event) => setRepeatPassword(event.target.value)}
+                        />
+                    </div>
+                </div>
+                <br />
+                <div>
+                    <label>Mobile Number:</label>
+                    <input
+                        placeholder='Mobile Number'
+                        type="tel"
+                        value={mobileNumber}
+                        onChange={(event) => setMobileNumber(event.target.value)}
+                    />
+                </div>
+                <br />
+                <div>
+                    <label>Date of Birth:</label>
+                    <div>
+                        <input
+                            type="text"
+                            value={birthDay}
+                            onChange={(event) => setBirthDay(event.target.value)}
+                            placeholder="DD"
+                            maxLength="2"
+                        />
+                        <input
+                            type="text"
+                            value={birthMonth}
+                            onChange={(event) => setBirthMonth(event.target.value)}
+                            placeholder="MM"
+                            maxLength="2"
+                        />
+                        <input
+                            type="text"
+                            value={birthYear}
+                            onChange={(event) => setBirthYear(event.target.value)}
+                            placeholder="YYYY"
+                            maxLength="4"
+                        />
+                    </div>
+                </div>
+                <br />
+                <div>
+                    <label>Research Paper (.pdf)*:</label>
+                    <input
+                        type="file"
+                        id="fileInput"
+                        onChange={handleFileChange}
+                        accept=".pdf"
+                        style={{ display: 'none' }}
+                    />
+                    {researchPaper == null ?
+                        <Button className="nav-button" onClick={handleButtonClick} >Upload File</Button>
+                        :
+                        <Button className="nav-button-2" style={{ color: "#fff", background: "#11111" }} disabled>{filename}</Button>
+                    }
 
-        <Popup open={open} handleClose={handleClose}/>
+
+                </div>
+                <br />
+                <span>Note: Kindly fill the details carefully as it will be printed on the award trophy, certificates, etc.</span>
+                <br />
+                <div>
+                    {researchPaper == null ?
+                        <Button className="nav-button" id="submit-button" disabled>Submit Application</Button>
+                        :
+                        <Button className="nav-button" id="submit-button-2" onClick={handleSubmit}>{loading === true ? "Loading..." : "Submit Application"}</Button>
+                    }
+
+                </div>
+            </form>
+
+            <Popup open={open} handleClose={handleClose} />
         </>
     );
 }
---
+++ src/utils/services.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { FORM_ENDPOINT } from "./routes";
+
+export const BASE_URL = "https://himashusirapi-production.up.railway.app"; //devlopment//
+
+
+axios.defaults.baseURL = BASE_URL;
+const token = localStorage.getItem("token");
+
+
+export async function SUBMITFORM(data) {
+    try {
+  
+      const response = await axios.post(FORM_ENDPOINT, data, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          // Authorization: "Bearer " + token,
+        },
+      });
+      if (response.status === 200) {
+        return response;
+      } else {
+        return response.data;
+      }
+  
+    } catch (err) {
+      console.log(err);
+      if (err.response) throw err.response.data;
+      else throw err.message;
+    }
+  }
