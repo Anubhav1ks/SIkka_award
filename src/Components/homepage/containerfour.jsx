277c6997f5fbab5e59682225bc3d169063e3f76b
Anubhav Sharma <annu992002@gmail.com>
2023-04-26 11:11:45 +0530

    update

--- src/Components/homepage/containerfour.jsx
+++ src/Components/homepage/containerfour.jsx
@@ -6,18 +6,16 @@ import Makarand from "../../Assets/images/Makarand.jpg";
 import Jatin from "../../Assets/images/Jatin.jpg";
 import AVM from "../../Assets/images/AVM.jpg";
 
-
 const FourContainer = () => {
   const [title, settitle] = React.useState("");
   const [discription, setdiscription] = React.useState("");
   const [img, setimg] = React.useState("");
 
-
   const [open, setOpen] = React.useState(false);
-  const handleOpen = (t,d,img) => {
+  const handleOpen = (t, d, img) => {
     settitle(t);
-    setimg(img)
-    setdiscription(d)
+    setimg(img);
+    setdiscription(d);
     setOpen(true);
   };
   const handleClose = () => setOpen(false);
@@ -68,16 +66,16 @@ const FourContainer = () => {
                     associated with TV Today Group as an Assistant Producer and
                     anchor...
                   </p>
-                <div
-                  className="read-more"
-                  onClick={() =>
-                    handleOpen(
-                      "Jatin Singh",
-                      "The Founder & Managing Director at Skymet Weather Services Pvt Ltd, he is in charge of the overall growth strategy of the company, technology innovation, operations and business dimensions. Prior to becoming an entrepreneur, Jatin was associated with TV Today Group as an Assistant Producer and anchor. Before joining the group, he was a reporter with ANI/Reuters handling multiple verticals such as National Broadcasting Corporation (New Delhi and Jammu & Kashmir) and a news bulletin carried in the United States, the South Asian Newsline. He was also an Associate Producer for the Singapore-based 24-hour news network Channel News Asia."
-                      ,Jatin
-                    )
-                  }
-                >
+                  <div
+                    className="read-more"
+                    onClick={() =>
+                      handleOpen(
+                        "Jatin Singh",
+                        "The Founder & Managing Director at Skymet Weather Services Pvt Ltd, he is in charge of the overall growth strategy of the company, technology innovation, operations and business dimensions. Prior to becoming an entrepreneur, Jatin was associated with TV Today Group as an Assistant Producer and anchor. Before joining the group, he was a reporter with ANI/Reuters handling multiple verticals such as National Broadcasting Corporation (New Delhi and Jammu & Kashmir) and a news bulletin carried in the United States, the South Asian Newsline. He was also an Associate Producer for the Singapore-based 24-hour news network Channel News Asia.",
+                        Jatin
+                      )
+                    }
+                  >
                     Read More
                   </div>
                 </div>
@@ -95,16 +93,16 @@ const FourContainer = () => {
                     studies in management and more than 14 years of
                     experience...
                   </p>
-                <div
-                  className="read-more"
-                  onClick={() =>
-                    handleOpen(
-                      "Yogesh Patil",
-                      "A meteorology and weather instrumentation expert, balanced by understanding risk to create opportunities and articulate ways to best leverage weather information across multiple platforms. He is an engineer by training, followed by studies in management and more than 14 years of experience"
-                      ,Yogesh
-                    )
-                  }
-                >
+                  <div
+                    className="read-more"
+                    onClick={() =>
+                      handleOpen(
+                        "Yogesh Patil",
+                        "A meteorology and weather instrumentation expert, balanced by understanding risk to create opportunities and articulate ways to best leverage weather information across multiple platforms. He is an engineer by training, followed by studies in management and more than 14 years of experience",
+                        Yogesh
+                      )
+                    }
+                  >
                     Read More
                   </div>
                 </div>
@@ -121,16 +119,16 @@ const FourContainer = () => {
                     Weather Services Pvt Ltd. Armed with nearly three decades of
                     extensive research and expertise in predictive analysis,...
                   </p>
-                <div
-                  className="read-more"
-                  onClick={() =>
-                    handleOpen(
-                      "Makarand Kulkarni",
-                      "A veteran and a visionary in agri-weather risk management, Makarand Kulkarni is leading the development of cutting-edge technology as the Chief Product Planning & Quality at Skymet Weather Services Pvt Ltd. Armed with nearly three decades of extensive research and expertise in predictive analysis,"
-                      ,Makarand
-                    )
-                  }
-                >
+                  <div
+                    className="read-more"
+                    onClick={() =>
+                      handleOpen(
+                        "Makarand Kulkarni",
+                        "A veteran and a visionary in agri-weather risk management, Makarand Kulkarni is leading the development of cutting-edge technology as the Chief Product Planning & Quality at Skymet Weather Services Pvt Ltd. Armed with nearly three decades of extensive research and expertise in predictive analysis,",
+                        Makarand
+                      )
+                    }
+                  >
                     Read More
                   </div>
                 </div>
@@ -149,16 +147,16 @@ const FourContainer = () => {
                     type specimen book. Lorem Ipsum is simply dummy text of the
                     printing and typesetting industry...
                   </p>
-                <div
-                  className="read-more"
-                  onClick={() =>
-                    handleOpen(
-                      "AVM GP Sharma",
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry"
-                    ,AVM
-                    )
-                  }
-                >
+                  <div
+                    className="read-more"
+                    onClick={() =>
+                      handleOpen(
+                        "AVM GP Sharma",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+                        AVM
+                      )
+                    }
+                  >
                     Read More
                   </div>
                 </div>
@@ -211,8 +209,8 @@ const FourContainer = () => {
                   onClick={() =>
                     handleOpen(
                       "Jatin Singh",
-                      "The Founder & Managing Director at Skymet Weather Services Pvt Ltd, he is in charge of the overall growth strategy of the company, technology innovation, operations and business dimensions. Prior to becoming an entrepreneur, Jatin was associated with TV Today Group as an Assistant Producer and anchor. Before joining the group, he was a reporter with ANI/Reuters handling multiple verticals such as National Broadcasting Corporation (New Delhi and Jammu & Kashmir) and a news bulletin carried in the United States, the South Asian Newsline. He was also an Associate Producer for the Singapore-based 24-hour news network Channel News Asia."
-                    ,Jatin
+                      "The Founder & Managing Director at Skymet Weather Services Pvt Ltd, he is in charge of the overall growth strategy of the company, technology innovation, operations and business dimensions. Prior to becoming an entrepreneur, Jatin was associated with TV Today Group as an Assistant Producer and anchor. Before joining the group, he was a reporter with ANI/Reuters handling multiple verticals such as National Broadcasting Corporation (New Delhi and Jammu & Kashmir) and a news bulletin carried in the United States, the South Asian Newsline. He was also an Associate Producer for the Singapore-based 24-hour news network Channel News Asia.",
+                      Jatin
                     )
                   }
                 >
@@ -235,8 +233,8 @@ const FourContainer = () => {
                   onClick={() =>
                     handleOpen(
                       "Yogesh Patil",
-                      "A meteorology and weather instrumentation expert, balanced by understanding risk to create opportunities and articulate ways to best leverage weather information across multiple platforms. He is an engineer by training, followed by studies in management and more than 14 years of experience"
-                      ,Yogesh
+                      "A meteorology and weather instrumentation expert, balanced by understanding risk to create opportunities and articulate ways to best leverage weather information across multiple platforms. He is an engineer by training, followed by studies in management and more than 14 years of experience",
+                      Yogesh
                     )
                   }
                 >
@@ -261,8 +259,8 @@ const FourContainer = () => {
                   onClick={() =>
                     handleOpen(
                       "Makarand Kulkarni",
-                      "A veteran and a visionary in agri-weather risk management, Makarand Kulkarni is leading the development of cutting-edge technology as the Chief Product Planning & Quality at Skymet Weather Services Pvt Ltd. Armed with nearly three decades of extensive research and expertise in predictive analysis,"
-                      ,Makarand
+                      "A veteran and a visionary in agri-weather risk management, Makarand Kulkarni is leading the development of cutting-edge technology as the Chief Product Planning & Quality at Skymet Weather Services Pvt Ltd. Armed with nearly three decades of extensive research and expertise in predictive analysis,",
+                      Makarand
                     )
                   }
                 >
@@ -274,20 +272,19 @@ const FourContainer = () => {
                 <br />
                 <span>AVM GP Sharma</span>
                 <p>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it to make a type
-                  specimen book. Lorem Ipsum is simply dummy text of the
-                  printing and typesetting industry...
+                  Mr Sharma, President of Metrology & Climate Change, Skymet
+                  Weather Services, he is a retired India Airforce Officer and
+                  headed the metrological branch while serving at Air
+                  Headquarters, New Delhi. He has been at Skymet for nearly 10
+                  years.​
                 </p>
                 <div
                   className="read-more"
                   onClick={() =>
                     handleOpen(
                       "AVM GP Sharma",
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry"
-                    ,AVM
+                      "Mr Sharma, President of Metrology & Climate Change, Skymet Weather Services, he is a retired India Airforce Officer and headed the metrological branch while serving at Air Headquarters, New Delhi. He has been at Skymet for nearly 10 years.​",
+                      AVM
                     )
                   }
                 >
@@ -301,9 +298,9 @@ const FourContainer = () => {
 
       {open ? (
         <BasicModal
-        title={title}
-        img={img}
-        discription={discription}
+          title={title}
+          img={img}
+          discription={discription}
           handleOpen={handleOpen}
           open={open}
           handleClose={handleClose}
